472bc568dc8731ec6c3e81ccad82fdc6d9a51173
Vedant Lahane <vedantanillahane@gmail.com>
2025-01-12 00:10:51 +0530

    Update CORS configuration to use the correct frontend URL

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: "https://vedantlahane.vercel.com", // Update this to your frontend's deployed URL (e.g., https://your-frontend.vercel.app) when ready
+  origin: "https://vedantlahane.vercel.app", // Update this to your frontend's deployed URL (e.g., https://your-frontend.vercel.app) when ready
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionSuccessStatus: 200,
