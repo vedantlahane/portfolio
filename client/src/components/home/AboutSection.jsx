d2d73bb5b400059146e5994bc7f4f1b744d1f69c
Vedant Lahane <vedantanillahane@gmail.com>
2025-05-02 01:10:44 +0530

    Enhance padding in AboutSection for better responsiveness

--- client/src/components/home/AboutSection.jsx
+++ client/src/components/home/AboutSection.jsx
@@ -10,7 +10,7 @@ const AboutSection = () => {
   }, []);
 
   return (
-    <section className="py-16 bg-transparent">
+    <section className="py-16 px-4 sm:px-6 lg:px-8 bg-transparent">
       <div className=" mx-auto px-4 sm:px-8">
         {/* Header */}
         <motion.div
